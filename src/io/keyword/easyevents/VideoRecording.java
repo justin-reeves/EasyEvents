940e93ad2455956162c93c7dd2c73879bf60c978
Justin Reeves <kjryan.reeves@gmail.com>
2021-03-26 14:09:32 -0700

    Setup of initial project structure with base classes

---
+++ src/io/keyword/easyevents/VideoRecording.java
@@ -0,0 +1,29 @@
+package io.keyword.easyevents;
+
+/**
+ * Class Description
+ * <p>
+ * Created By: Justin Reeves, Pasang Sherpa, Xiaotian Wang (Xander)
+ * Created On: 3/26/2021
+ */
+class VideoRecording {
+
+    // FIELDS
+
+
+    // CONSTRUCTORS
+
+
+    // BUSINESS METHODS
+
+
+    // ACCESSOR METHODS
+
+
+    // HELPER METHODS
+
+
+    // OVERRIDES
+
+
+}
