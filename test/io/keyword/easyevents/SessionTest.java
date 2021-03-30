43216b8349882c375724b011de387df7ba06596f
shpasang <pasang.sherpa2044@gmail.com>
2021-03-30 15:59:37 -0700

    Setting up controller.

---
+++ test/io/keyword/easyevents/SessionTest.java
@@ -0,0 +1,20 @@
+package io.keyword.easyevents;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SessionTest {
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testStartSession_withNoArgs() {
+        Session session = Session.getInstance();
+        session.startSession();
+    }
+}
